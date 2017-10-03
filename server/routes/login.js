8e5830797103c291c622abc32695698f0cd619e6
slaymance <shane.laymance@gmail.com>
2017-10-03 16:39:14 -0700

    Implement serialization for logging in users

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,15 +4,18 @@ var LocalStrategy = require('passport-local').Strategy;
 passport.use(new LocalStrategy(
   function(username, password, done) {
     console.log('Passport');
-    return done(null, {blah: 'some string'});
+    return done(null, {id: 1234, username: 'shane'});
   }
 ));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
 
 const login = {
   post: passport.authenticate('local', {
     successRedirect: '/',
-  	failureRedirect: '/login',
-    session: false
+    failureRedirect: '/login',
   })
 };
 
