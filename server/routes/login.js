591e34e8d15c1f64ce06fe9e8b60f462ab8d4993
arsimr16 <arsimr16@g.holycross.edu>
2017-10-03 11:30:37 -0700

    resolve merge conflict in login.js

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,4 +1,27 @@
-const passport = require('passport');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+  	// verify callback finds a user that possessed a set of credentials
+  	User.findOne({username: username}, function(err, user) {
+  		// if there is an error with the server
+  		if (err) {
+  			return done(err);
+  		}
+      // if the username is incorrect
+  		if (!user) {
+  			return done(null, false, {message: 'Incorrect username.'});
+  		}
+  		// if the password in incorrect
+  		if (!user.validPassword(password)) {
+  			return done(null, false, {message: 'Incorrect password.'});
+  		}
+      // the credentials are valid; supply passport with authenticated user
+  		return done(null, user);
+  	});
+  }
+));
 
 var login = {
 	// the first argument specifies the strategy - local
