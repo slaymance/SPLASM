526fb2faa3535e05cd07676691a90a29ee850be3
Marcus Polk <marcuspolk96@gmail.com>
2017-10-02 18:26:30 -0700

    outlined test process

--- test/test.js
+++ test/test.js
@@ -1,6 +1,7 @@
-var chai = require('chai');  
-var assert = chai.assert;
+var expect = require('chai').expect;
 var sinon = require('sinon');
+
+// from: https://semaphoreci.com/community/tutorials/best-practices-for-spies-stubs-and-mocks-in-sinon-js
 /* The most common scenarios with spies (function wrappers that let you 'spy' on function calls.)
 	involve...
 Checking how many times a function was called
@@ -15,13 +16,33 @@ You can use them to trigger code paths that wouldn't otherwise trigger - such as
 You can use them to help test asynchronous code more easily
 */
 
-/*
 
+/*
+	shortly-express test setup: 
+		beforeEach test:
+			connect to DB.
+			clear DB tables.
+			create DB tables based on config file schema.
+			curious how the async works on the beforeeach.
+				how does it know when it's done?
+				Answer: done passed into BeforeEach function is called
+				as callback to app.listen.
+		Database Schema Tests: 
+			test that tables exist.
+			test for correct columns
+			test insertion and retrieval of tables.
+
+	Note: Test deletes the database. In this case it should
+	be ok since it's only deleting the local/test db.
+
+	Todo: Learn basics of mongo. Setup some tests just verifying
+	what's already there. Use similar specs from shortly express.
+	Same for SQL.
 */
 describe('Mongo DB', function() {
   describe('#indexOf()', function() {
-    it('should return -1 when the value is not present', function() {
-      assert.equal(-1, [1,2,3].indexOf(4));
+    it('contains a user table', function() {
+      var queryString = ""
     });
   });
 });
