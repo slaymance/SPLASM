eb4b1cb61a62d77eefa40db868785cc09871077f
Shweta Patrachari <shwetachari@gmail.com>
2017-10-02 19:24:30 -0700

    added percent courses completed stat

--- client/components/profile/Profile.jsx
+++ client/components/profile/Profile.jsx
@@ -1,20 +1,33 @@
 import React from 'react';
 
-const Profile = (props) =>
-  <div style={{wordWrap: 'break-word'}}>
-    <img src={props.user.picture} className="col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-1"/>
-    <div className="col-xs-12">
-      <h3>{props.user.name}</h3>
-      <div className="profileItem">
-        Member since: {[props.user.createdAt.slice(5, 10), props.user.createdAt.slice(0, 4)].join('-')}
+
+class Profile extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    let percentComplete = 100 * (this.props.user.courses.reduce((tot, course) => {
+      return course.status === '100%' ? tot + 1 : tot;
+    }, 0) / this.props.user.courses.length);
+
+    return (<div style={{wordWrap: 'break-word'}}>
+      <img src={this.props.user.picture} className="col-sm-12 col-sm-offset-0 col-xs-10 col-xs-offset-1"/>
+      <div className="col-xs-12">
+        <h3>{this.props.user.name}</h3>
+        <div className="profileItem">
+          Member since: {[this.props.user.createdAt.slice(5, 10), this.props.user.createdAt.slice(0, 4)].join('-')}
+        </div>
+        <div className="profileItem">
+          Interests: {this.props.user.interests.join(', ')}
+        </div>
+        <div className="profileItem">
+          Courses complete: {percentComplete}%
+        </div>
       </div>
-      <div className="profileItem">
-        Interests: {props.user.interests.join(', ')}
-      </div>
-      <div className="profileItem">
-        Statistics: (use props.user.courses)
-      </div>
-    </div>
-  </div>;
+    </div>)
+  }
+}
+
 
 export default Profile;
