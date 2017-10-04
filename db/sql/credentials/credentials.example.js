e9c64993d2bff9d3fb132c553dc0a56848042175
slaymance <shane.laymance@gmail.com>
2017-10-04 09:16:41 -0700

    Add git ignored credentials file for access to MySQL database

---
+++ db/sql/credentials/credentials.example.js
@@ -0,0 +1,11 @@
+/*
+* Create a copy of this file in the same directory and name the
+* file 'credentials.js'. Fill out your credentials to connect to your MySQL
+* database. 'credentials.js' will automatically be ignored by git.
+*/
+const credentials = {
+  username: '',
+  password: ''
+};
+
+module.exports = credentials;
