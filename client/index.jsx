dc5de578e9f80c04e315526997cac8ceb78362a7
arsimr16 <arsimr16@g.holycross.edu>
2017-10-04 13:04:38 -0700

    create components for signup and login

--- client/index.jsx
+++ client/index.jsx
@@ -2,6 +2,8 @@ import App from './components/App.jsx';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/style.css';
+import Signup from './components/signup/Signup.jsx';
+import Login from './components/login/Login.jsx';
 
 // start by making a get request
 // check if user has session
@@ -10,8 +12,10 @@ import './styles/style.css';
 
 console.log(window.location.pathname)
 
-if(window.location.pathname === '/login') {
-  //ReactDOM.render(<Login/>, document, getElementById('root'));
-} else {
-  ReactDOM.render(<App/>, document.getElementById('root'));
-}
+ReactDOM.render(<Login/>, document.getElementById('root'));
+
+// if(window.location.pathname === '/login') {
+//   //ReactDOM.render(<Login/>, document, getElementById('root'));
+// } else {
+//   ReactDOM.render(<App/>, document.getElementById('root'));
+// }
