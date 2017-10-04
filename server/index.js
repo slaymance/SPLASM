06bea5f0aa32faa1f7c9b26ac46c4368d8790db6
slaymance <shane.laymance@gmail.com>
2017-10-04 15:57:42 -0700

    Automatically redirect to login page if not logged in

--- server/index.js
+++ server/index.js
@@ -7,15 +7,22 @@ const router = require('./router.js');
 const sequelize = require('../db/sql/index.js');
 const env = require('dotenv').load();
 const path = require('path');
+const loggedIn = require('./middleware/loggedIn.js');
 
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.resolve(__dirname, '..', 'client')));
+app.use(express.static(path.resolve(__dirname, '..', 'client'), {
+  index: false
+}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(session({secret: 'splasm', resave: true, saveUninitialized: true})); //session secret
 app.use(passport.initialize());
 app.use(passport.session()); //persistent login sessions
+
+app.get('/', loggedIn, (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/index.html'));
+});
 
 app.use('/', router);
 
