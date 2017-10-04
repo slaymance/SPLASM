7d7c2e12b5585e56c55963623c9234cd6fb7c1f5
arsimr16 <arsimr16@g.holycross.edu>
2017-10-03 21:51:59 -0700

    implement db query inside local strategy

--- server/routes/login.js
+++ server/routes/login.js
@@ -4,33 +4,27 @@ const User = require('../../db/sql/models/User.js');
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    // User.findOne({
-    //   where: {username: username}
-    // }).then((err, userObj) => {
-    //   // userObj is the first item in the users table with a matching username
-    //   // handle error if necessary
-    //   if (err) {
-    //     return done(err);
-    //   }
-    //   // if the user does not exist
-    //   if (!userObj) {
-    //     return done(null, false, {message: 'Incorrect username.'});
-    //   }
-    //   // if the password does not match
-    //   if (!userObj.validPassword(hash)) {
-    //     return done(null, false, {message: 'Incorrect password.'});
-    //   }
-    //   // else the username and password are valid
-    //   return done(null, userObj);
-    // });
-
     User.findOne({
       where: {name: username}
-    }).then(user => {
-      console.log('user: ', JSON.parse(JSON.stringify(user)));
-      return done(null, {user: 'testing'});
-    })
-    // return done(null, {user: 'testing'});
+    }).then ((userObj) => {
+      // err handling does not work; userObj must be the first argument
+      // if (err) {
+      //   console.log('there is an error!!!', err);
+      // }
+
+      // if the user does not exist
+      if (!userObj) {
+        return done(null, false, {message: 'Incorrect username.'});
+      }
+      console.log(JSON.stringify(userObj));
+      // if password does not match
+      if (userObj.hash !== password) {
+        console.log('password does not match');
+        return done(null, false, {message: 'Incorrect password.'});
+      }
+      // else user and password match
+      return done(null, userObj);
+    });
   }
 ));
 
