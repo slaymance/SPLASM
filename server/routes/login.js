60877bf791c42d1db8550f7940048f437f7c0ec8
arsimr16 <arsimr16@g.holycross.edu>
2017-10-03 17:59:01 -0700

    resolve merge conflict

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,10 +1,36 @@
-var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../db/sql/models/User.js');
 
 passport.use(new LocalStrategy(
   function(username, password, done) {
-    console.log('Passport');
-    return done(null, {id: 1234, username: 'shane'});
+    // User.findOne({
+    //   where: {username: username}
+    // }).then((err, userObj) => {
+    //   // userObj is the first item in the users table with a matching username
+    //   // handle error if necessary
+    //   if (err) {
+    //     return done(err);
+    //   }
+    //   // if the user does not exist
+    //   if (!userObj) {
+    //     return done(null, false, {message: 'Incorrect username.'});
+    //   }
+    //   // if the password does not match
+    //   if (!userObj.validPassword(hash)) {
+    //     return done(null, false, {message: 'Incorrect password.'});
+    //   }
+    //   // else the username and password are valid
+    //   return done(null, userObj);
+    // });
+
+    User.findOne({
+      where: {name: username}
+    }).then(user => {
+      console.log('user: ', JSON.parse(JSON.stringify(user)));
+      return done(null, {user: 'testing'});
+    })
+    // return done(null, {user: 'testing'});
   }
 ));
 
