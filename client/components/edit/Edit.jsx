8afa201cfbc3bafe8bd0bcd63af02f9ff78d045a
arsimr16 <arsimr16@g.holycross.edu>
2017-10-06 10:20:18 -0700

    implement editProf function

--- client/components/edit/Edit.jsx
+++ client/components/edit/Edit.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
+const User = require('../../db/sql/models/User.js')
+
+// when this is rendered it will look something like this:
+// <Edit field="picture">
+//   Add your picture here (prompt message accessed by props.children)
+// </Edit>
 
 class Edit extends React.Component {
 	constructor(props) {
 		super(props);
+		//I'm not sure if I need this line or not
+		this.editProf = this.editProf.bind(this);
+	}
+
+	editProf() {
+		const change = prompt(this.props.children);
+		User.update(
+			{this.props.field: change},
+		{where: {name: /*access user name/id here*/}}
+		).then(result => {
+			console.log('result of update: ', result);
+		}).catch(err => {
+			console.error('update error: ', err);
+		});
 	}
 
 	render() {
 		return (
 			<div>
+			  <button type="button" onClick={this.editProf}></button>
 			</div>
 		);
 	}
