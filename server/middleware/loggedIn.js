0343089765e72fe191e63b0d52a2cef4208bf259
slaymance <shane.laymance@gmail.com>
2017-10-05 20:15:23 -0700

    Auto login users after signing up

--- server/middleware/loggedIn.js
+++ server/middleware/loggedIn.js
@@ -1,9 +1,46 @@
-const loggedIn = (req, res, next) => {
-  if (req.user) {
-    next();
-  } else {
-    res.redirect('/login');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const path = require('path');
+const User = require('../../db/sql/models/User.js');
+const bcrypt = require('bcryptjs');
+
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    User.findOne({
+      where: {name: username}
+    }).then ((userObj) => {
+      // if the user does not exist
+      if (!userObj) {
+        return done(null, false, {message: 'Incorrect username.'});
+      }
+      // if password does not match
+      if (!bcrypt.compareSync(password, userObj.hash)) {
+        console.log('password does not match');
+        return done(null, false, {message: 'Incorrect password.'});
+      }
+      // else user and password match
+      return done(null, userObj);
+    }).catch((err) => {
+      console.error(err);
+    });
   }
+));
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then((user) => {
+    done(null, user);
+  }).catch(error => console.error(error));
+});
+
+const loggedIn = {
+  login: passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+  })
 };
 
 module.exports = loggedIn;
