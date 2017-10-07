1a9a69d32dff2e160d41f70d53821b1154620016
slaymance <shane.laymance@gmail.com>
2017-10-06 20:44:38 -0700

    Fix bug with signup and login

--- client/components/login/Login.jsx
+++ client/components/login/Login.jsx
@@ -4,26 +4,24 @@ import { Link } from 'react-router-dom';
 const Login = () =>
   <div className="container login-container col-xs-4 col-xs-offset-4">
     <div className="col-xs-6 col-xs-offset-3">
-      <form className="login-form" action="/action_page.php">
-        <div className="imgcontainer">
-          <img src="../../images/defaultProfPic.png" alt="Avatar" className="avatar"/>
-        </div>
+      <div className="imgcontainer">
+        <img src="../../images/defaultProfPic.png" alt="Avatar" className="avatar"/>
+      </div>
 
-        <div>
-          <input className="username-input" type="text" placeholder="Username" required></input>
+      <form className="login-form" action="/login" method="post">
+        <input className="username-input" type="text" placeholder="Username" name="username" required></input>
 
-          <input className="password-input" type="password" placeholder="Password" required></input>
+        <input className="password-input" type="password" name="password" placeholder="Password" required></input>
 
-          <button className="loginbtn" type="submit">Login</button>
-        </div>
-
-        <div>
-          <Link to ="/">
-            <button className="cancelbtn" type="button">Cancel</button>
-          </Link>
-          <span className="redir">New to nodebook? <Link to="/signup">Sign up »</Link></span>
-        </div>
+        <button className="loginbtn" type="submit">Login</button>
       </form>
+
+      <div>
+        <Link to ="/">
+          <button className="cancelbtn" type="button">Cancel</button>
+        </Link>
+        <span className="redir">New to nodebook? <Link to="/signup">Sign up »</Link></span>
+      </div>
     </div>
   </div>;
 
--- client/components/signup/Signup.jsx
+++ client/components/signup/Signup.jsx
@@ -4,26 +4,23 @@ import { Link } from 'react-router-dom';
 const Signup = () =>
   <div className="container login-container col-xs-4 col-xs-offset-4">
     <div className="col-xs-6 col-xs-offset-3">
-      <form className="login-form" action="/action_page.php">
-        <div className="imgcontainer">
-          <img src="../../images/defaultProfPic.png" alt="Avatar" className="avatar"/>
-        </div>
+      <div className="imgcontainer">
+        <img src="../../images/defaultProfPic.png" alt="Avatar" className="avatar"/>
+      </div>
+      <form className="login-form" action="/signup" method="post">
+        <input className="username-input" type="text" name="username" placeholder="Username" required></input>
 
-        <div>
-          <input className="username-input" type="text" placeholder="Username" required></input>
+        <input className="password-input" type="password" name="password" placeholder="Password" required></input>
 
-          <input className="password-input" type="password" placeholder="Password" required></input>
-
-          <button className="loginbtn" type="submit">Signup</button>
-        </div>
-
-        <div>
-          <Link to ="/">
-            <button className="cancelbtn" type="button">Cancel</button>
-          </Link>
-          <span className="redir">Already a user? <Link to="/login">Login »</Link></span>
-        </div>
+        <button className="loginbtn" type="submit">Signup</button>
       </form>
+
+      <div>
+        <Link to ="/">
+          <button className="cancelbtn" type="button">Cancel</button>
+        </Link>
+        <span className="redir">Already a user? <Link to="/login">Login »</Link></span>
+      </div>
     </div>
   </div>;
 
