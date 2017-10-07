b7545a7b3f154aacfa7b3337e8c8ec00da7b23cf
slaymance <shane.laymance@gmail.com>
2017-10-06 19:00:18 -0700

    Rough draft of login page

--- client/components/login/Login.jsx
+++ client/components/login/Login.jsx
@@ -1,37 +1,60 @@
 import React from 'react';
 
-class Login extends React.Component {
-  constructor(props) {
-  	super(props);
-  }
+const Login = () =>
+  <form action="/action_page.php">
+    <div className="imgcontainer">
+      <img src="../../images/defaultProfPic.png" alt="Avatar" className="avatar"/>
+    </div>
 
-  render() {
-  	return (
-  		<div>
-  		  <h1>Welcome to Splasm!</h1>
-  		  <p>Sign in to view your profile.</p>
-	  		<form action="/login" method="post">
-	  		  <div>
-	  		  	<label>Username:</label>
-	  		  	<input type="text" name="username"/>
-	  		  </div>
-	  		  <div>
-	  		    <label>Password:</label>
-	  		    <input type="password" name="password"/>
-	  		  </div>
-	  		  <div>
-	  		  	<input type="submit" value="Log In"/>
-	  		  </div>
-	  		</form>
-	  		<h3>New user?</h3>
-	  		<form action="/signup" method="get">
-	  		  <div>
-	  		    <input type="submit" value="Sign Up"/>
-	  		  </div>
-	  		</form>
-  		</div>
-  	);
-  }
-}
+    <div className="container">
+      <label><b>Username</b></label>
+      <input type="text" placeholder="Enter Username" name="uname" required></input>
+
+      <label><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+      <button type="submit">Login</button>
+    </div>
+
+    <div className="container">
+      <button type="button" className="cancelbtn">Cancel</button>
+      <span className="psw">Forgot <a href="#">password?</a></span>
+    </div>
+  </form>;
+
+
+// class Login extends React.Component {
+//   constructor(props) {
+//   	super(props);
+//   }
+//
+//   render() {
+//   	return (
+//   		<div>
+//   		  <h1>Welcome to Splasm!</h1>
+//   		  <p>Sign in to view your profile.</p>
+// 	  		<form action="/login" method="post">
+// 	  		  <div>
+// 	  		  	<label>Username:</label>
+// 	  		  	<input type="text" name="username"/>
+// 	  		  </div>
+// 	  		  <div>
+// 	  		    <label>Password:</label>
+// 	  		    <input type="password" name="password"/>
+// 	  		  </div>
+// 	  		  <div>
+// 	  		  	<input type="submit" value="Log In"/>
+// 	  		  </div>
+// 	  		</form>
+// 	  		<h3>New user?</h3>
+// 	  		<form action="/signup" method="get">
+// 	  		  <div>
+// 	  		    <input type="submit" value="Sign Up"/>
+// 	  		  </div>
+// 	  		</form>
+//   		</div>
+//   	);
+//   }
+// }
 
 export default Login;
