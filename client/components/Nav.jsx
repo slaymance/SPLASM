c6a99418f0932c24491de88692c75da9e2639353
slaymance <shane.laymance@gmail.com>
2017-10-06 18:43:09 -0700

    Implement working Navbar

--- client/components/Nav.jsx
+++ client/components/Nav.jsx
@@ -49,49 +49,55 @@ class Nav extends React.Component {
     return (<header>
       <nav className="navbar navbar-default main-nav">
         <div className="container-fluid">
-          { !this.props.user
-            ? <ul className="nav navbar-nav navbar-right action-list">
-              <li className="action">
-                <Link to="/signup">
-                  <span className="glyphicon glyphicon-user"></span> Sign Up
-                </Link>
-              </li>
-              <li className="action">
-                <Link to="/login">
-                  <span className="glyphicon glyphicon-log-in"></span> Login
-                </Link>
-              </li>
-            </ul>
-            : <ul className="nav navbar-nav navbar-right action-list">
-              <li><Link to={'/profile/users/' +
-                this.props.user}>Profile</Link></li>
-              <li>
-                <Link to="" onClick={this.logout}>
-                  <span className="glyphicon glyphicon-log-out"></span> Logout
-                </Link>
-              </li>
-            </ul>
-          }
+          <ul className="nav navbar-nav action-list">
+            <li className="container col-xs-2">
+              <div className="input-group stylish-input-group">
+                <span className="input-group-addon">
+                  <Link replace to={'/search/users/' + this.state.userVal}>
+                    <button type="submit">
+                      <span className="glyphicon glyphicon-search"></span>
+                    </button>
+                  </Link>
+                </span>
+                <input list="users" name="user" value={this.state.userVal} onChange={this.changeUserVal.bind(this)} type="text" className="form-control" placeholder="Traverse profiles"/>
+              </div>
+            </li>
+            { !this.props.user
+              ? <div className="nav navbar-nav navbar-right action-list inline-list">
+                <li className="action">
+                  <Link to="/signup">
+                    <span className="glyphicon glyphicon-user"></span> Sign Up
+                  </Link>
+                </li>
+                <li className="action">
+                  <Link to="/login">
+                    <span className="glyphicon glyphicon-log-in"></span> Login
+                  </Link>
+                </li>
+              </div>
+              : <div className="nav navbar-nav navbar-right action-list inline-list">
+                <li>
+                  <Link to={'/profile/users/' + this.props.user}>
+                    <span className="glyphicon glyphicon-tree-deciduous"></span>  Profile
+                  </Link></li>
+                <li>
+                  <Link to="" onClick={this.logout}>
+                    <span className="glyphicon glyphicon-log-out"></span> Logout
+                  </Link>
+                </li>
+              </div>
+            }
+          </ul>
           <div className="navbar-header">
-            <a href="/" className="navbar-brand">NodeBook</a>
+            <a href="/" className="navbar-brand">nodebook</a>
           </div>
         </div>
       </nav>
-      <nav classname="navbar navbar-default second-nav">
-        <ul className="nav navbar-nav navbar-right">
-          <li>
-            <input list="users" name="user" value={this.state.userVal} onChange={this.changeUserVal.bind(this)}/>
-            <datalist id="users">
-              {this.state.usersList.map((username, index) => {
-                return (<option key={index} value={username}/>);
-              })}
-            </datalist>
-          </li>
-          <li>
-            <Link replace to={'/search/users/' + this.state.userVal}><button>Search</button></Link>
-          </li>
-        </ul>
-      </nav>
+      <datalist id="users">
+        {this.state.usersList.map((username, index) => {
+          return (<option key={index} value={username}/>);
+        })}
+      </datalist>
     </header>);
   }
 }
