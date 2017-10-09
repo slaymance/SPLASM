d4822747a061cde4063372607d552d227e52c37f
Shweta Patrachari <shwetachari@gmail.com>
2017-10-09 10:28:00 -0700

    added responsive styling to login and signup components

--- client/components/login/Login.jsx
+++ client/components/login/Login.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import icon from '../../images/defaultProfPic.png';
 
 const Login = () =>
-  <div className="container login-container col-xs-4 col-xs-offset-4">
-    <div className="col-xs-6 col-xs-offset-3">
+  <div className="container login-container col-sm-6 col-sm-offset-3 col-xs-12 col-xs-offset-0">
+    <div className="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2">
       <div className="imgcontainer">
-        <img src={icon} alt="Avatar" className="avatar"/>
+        <img src={icon} alt="Avatar" className="avatar avatar col-sm-10 col-sm-offset-1 col-xs-6 col-xs-offset-3"/>
       </div>
 
       <form className="login-form" action="/login" method="post">
@@ -15,13 +15,13 @@ const Login = () =>
         <input className="password-input" type="password" name="password" placeholder="Password" required></input>
 
         <button className="loginbtn" type="submit">Login</button>
+        <Link to="/">
+          <button className="cancelbtn" type="button">Cancel</button>
+        </Link>
       </form>
 
       <div>
-        <Link to ="/">
-          <button className="cancelbtn" type="button">Cancel</button>
-        </Link>
-        <span className="redir">New to nodebook? <Link to="/signup">Sign up »</Link></span>
+        <span className="redir col-xs-12">New to nodebook?<br/><Link to="/signup">Sign up »</Link></span>
       </div>
     </div>
   </div>;
--- client/components/signup/Signup.jsx
+++ client/components/signup/Signup.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 import icon from '../../images/nodebook_icon_mono.png';
 
 const Signup = () =>
-  <div className="container login-container col-xs-4 col-xs-offset-4">
-    <div className="col-xs-6 col-xs-offset-3">
+  <div className="container login-container col-sm-6 col-sm-offset-3 col-xs-12 col-xs-offset-0">
+    <div className="col-xs-12 col-xs-offset-0 col-sm-8 col-sm-offset-2">
       <div className="imgcontainer">
-        <img src={icon} alt="Avatar" className="signup-avatar"/>
+        <img src={icon} alt="Avatar" className="signup-avatar avatar col-sm-10 col-sm-offset-1 col-xs-6 col-xs-offset-3"/>
       </div>
       <form className="login-form" action="/signup" method="post">
         <input className="username-input" type="text" name="username" placeholder="Username" required></input>
@@ -18,9 +18,9 @@ const Signup = () =>
 
       <div>
         <Link to ="/">
-          <button className="cancelbtn" type="button">Cancel</button>
+          <button className="cancelbtn col-xs-12 text-center" type="button">Cancel</button>
         </Link>
-        <span className="redir">Already a user? <Link to="/login">Login »</Link></span>
+        <span className="redir col-xs-12">Already a user?<br/><Link to="/login">Login »</Link></span>
       </div>
     </div>
   </div>;
